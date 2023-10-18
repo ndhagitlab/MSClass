f5630035df72b606d52ebe34ca65c136c4bdc435
ndha vcbs <ndha@vcbs.com.vn>
2023-10-18 12:53:26 +0700

    My C#

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using VCBS_SERVER;
+
+Console.WriteLine("Hello, Nguyen Duc Ha");
+int i = 0;
+while (i < 5)
+{
+    Console.WriteLine(i);
+    i++;
+}
+Console.WriteLine("");
+Console.WriteLine("Use While for loop printing 1-5");
+int j = 0;
+do
+{
+    Console.WriteLine(j);
+    j++;
+} while (j < 5);
+
+Console.WriteLine("");
+Console.WriteLine("Use Do for loop printing 1-5 ");
+
+int[] numbers = new int[5];
+
+for (int m = 0; m < 5; m++)
+{
+    numbers[m] = m;
+    Console.WriteLine(m);
+}
+Console.WriteLine("Gan mang du lieu use For: ");
+Console.WriteLine(String.Join(",", numbers));
+Console.WriteLine("");
+
+Server VCBS_server = new VCBS_SERVER.Server("","","");
+
+Console.WriteLine("Use class VCBS_Server to print");
+
+VCBS_server.svr_name = "Trading Core - 01";
+VCBS_server.svr_ipaddress = "192.168.1.100";
+VCBS_server.svr_type = "DELL EMC R650";
+
+VCBS_server.myserver();
+
+Thread.Sleep(5000);
---
+++ Server.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCBS_SERVER
+{
+    public class Server
+    {
+        public string svr_name { get; set; }
+        public string svr_ipaddress { get; set; }
+        public string svr_type { get; set; }
+        public Server(string svr_name, string svr_ipaddress, string svr_type)
+        {
+            this.svr_name = svr_name;
+            this.svr_ipaddress = svr_ipaddress;
+            this.svr_type = svr_type;
+        }
+       public void myserver()
+        {
+            Console.WriteLine("VCBS server is: " + svr_name + " - IP address :" + svr_ipaddress + " - Type Server :" +svr_type );
+        }
+    }
+}
